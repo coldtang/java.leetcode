a2450635d5c7fb856a0e613696a56983db09547c
ABF2557E60EEF2B5F5E068F0AD74B6D2 <ABF2557E60EEF2B5F5E068F0AD74B6D2@163.com>
2021-03-16 23:24:47 +0800

    最大数

--- src/main/java/com/tang/sort/LargestNumberSorter.java
+++ src/main/java/com/tang/sort/LargestNumberSorter.java
@@ -1,15 +1,64 @@
 package com.tang.sort;
 
+import static com.tang.sort.QuickSorter.swap;
+
 /**
  * 最大数
+ *
  * @author coldtang
  */
 public class LargestNumberSorter {
     public static void main(String[] args) {
-
+        int[] data = new int[]{3, 30, 34, 5, 32};
+        System.out.println(largestNumber(data));
     }
 
-//    public String largestNumber(int[] nums) {
-//
-//    }
+    public static String largestNumber(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        if (nums[0] == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 降序三路快排
+     *
+     * @param data
+     * @param lo
+     * @param hi
+     */
+    private static void sort(int[] data, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+
+        int pivot = data[hi];
+
+        int less = lo;
+        int great = hi;
+
+        int i = lo;
+        while (i <= great) {
+            String xy = data[i] + "" + pivot;
+            String yx = pivot + "" + data[i];
+            if (xy.compareTo(yx) > 0) {
+                swap(data, less, i);
+                i++;
+                less++;
+            } else if (xy.compareTo(yx) < 0) {
+                swap(data, i, great);
+                great--;
+            } else {
+                i++;
+            }
+        }
+
+        sort(data, lo, less - 1);
+        sort(data, great + 1, hi);
+    }
 }
